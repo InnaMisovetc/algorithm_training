9ac9e93d53c29afb706e362b3829f6e53a1d6c09
Inna Misovetc <inna.misovetc@skoltech.ru>
2021-08-21 21:59:51 +0300

    Added contest1 A solution

---
+++ yandex_algorithm_training/contest1/A-Conditioner.py
@@ -0,0 +1,24 @@
+def temp_after_one_hour(t_room, t_cond, mode):
+    if mode == 'fan':
+        return t_room
+    elif mode == 'auto':
+        return t_cond
+    elif mode == 'freeze':
+        if t_cond < t_room:
+            return t_cond
+        else:
+            return t_room
+    elif mode == 'heat':
+        if t_cond > t_room:
+            return t_cond
+        else:
+            return t_room
+
+
+def main():
+    t_room, t_cond = map(int, input().split(' '))
+    mode = input()
+    print(temp_after_one_hour(t_room, t_cond, mode))
+
+
+main()
