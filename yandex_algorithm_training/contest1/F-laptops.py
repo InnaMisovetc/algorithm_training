d31e28d9b573f8c479948a9253b036d7b9a29515
Inna Misovetc <inna.misovetc@skoltech.ru>
2021-09-09 23:01:23 +0300

    added  F task contest 1

---
+++ yandex_algorithm_training/contest1/F-laptops.py
@@ -0,0 +1,20 @@
+def main():
+    a1, b1, a2, b2 = map(int, input().split(' '))
+    x = [a1, b1]
+    y = [a2, b2]
+    s = 2 * (10**6)
+    for i in range(2):
+        for k in range(2):
+            new_s1 = max(x.pop(i), y.pop(k))
+            new_s2 = x[0] + y[0]
+            new_s = new_s1 * new_s2
+            if new_s <= s:
+                s = new_s
+                s1 = new_s1
+                s2 = new_s2
+            x = [a1, b1]
+            y = [a2, b2]
+    print(s1, s2)
+
+
+main()
